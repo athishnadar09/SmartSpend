8185d87841c13945bb1ca8c91e4b6dbf6c997d75
Athish Nadar <athishnadar1@gmail.com>
2026-09-17 03:20:56 +0530

    Expense object has been created

---
+++ src/Expense.java
@@ -0,0 +1,13 @@
+public class Expense {
+        double amount;
+        String category;
+        String description;
+        String date;
+
+        public Expense (double amount, String category, String description, String date){
+            this.amount = amount;
+            this.category = category;
+            this.description = description;
+            this.date = date;
+        }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
 public class Main {
-    
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do{
+            System.out.println("====SMART SPEND====");
+            System.out.println("1.Add Expense");
+            System.out.println("2.View Expense");
+            System.out.println("3.Total Expense");
+            System.out.println("4.Exit");
+            System.out.println("====================");
+            System.out.println("Enter the choice");
+            choice = sc.nextInt();
+        }while(choice!=4);
+    }
 }
