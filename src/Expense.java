eec90de7b8f6a8b72389697376610c06b2847bb1
Athish Nadar <athishnadar1@gmail.com>
2026-09-20 03:17:18 +0530

    encapsulation added

--- src/Expense.java
+++ src/Expense.java
@@ -1,13 +1,30 @@
 public class Expense {
-        double amount;
-        String category;
-        String description;
-        String date;
+        private double amount;
+        private String category;
+        private String description;
+        private String date;
 
         public Expense (double amount, String category, String description, String date){
             this.amount = amount;
             this.category = category;
             this.description = description;
             this.date = date;
+        }
+
+        //Encapsulation
+        public double getAmount(){
+            return amount;
+        }
+
+        public String getCategory(){
+            return category;
+        }
+
+        public String getDescription(){
+            return description;
+        }
+
+        public String getDate(){
+            return date;
         }
 }
