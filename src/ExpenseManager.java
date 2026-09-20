6618e85f0085d492345e9efebb0584c09742b134
Athish Nadar <athishnadar1@gmail.com>
2026-09-20 12:53:32 +0530

    added addExpense and viewExpense

--- src/ExpenseManager.java
+++ src/ExpenseManager.java
@@ -2,6 +2,24 @@ import java.util.ArrayList;
 
 public class ExpenseManager {
     static ArrayList<Expense> expenses = new ArrayList<>();
-    public void addExpense()
+    public void addExpense(Expense expense){
+        expenses.add(expense);
+    }
+    public void viewExpense(){
+        for(Expense expense : expenses){
+            System.out.println("Amount : "+expense.getAmount());
+            System.out.println("Category : "+expense.getCategory());
+            System.out.println("Description : "+expense.getDescription());
+            System.out.println("Date : "+expense.getDate());
+            System.out.println("----------------------------------------");
+        }
+    }
+    public void totalExpense(){
+        double sum = 0;
+        for(Expense expense : expenses){
+            sum += expense.getAmount();
+        }
+        System.out.println("Total Expense : ₹"+sum);
+    }
     
 }
